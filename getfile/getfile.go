28ebd449debf809e059433184e58e55086e0b851
yoshikawa0711 <b1017166@fun.ac.jp>
2020-05-18 18:22:51 +0900

    [add]指定されたディレクトリ以下のファイルのパスを取得する関数を作成

--- getfile/getfile.go
+++ getfile/getfile.go
@@ -1,5 +1,26 @@
 package getfile
 
-func GetFiles(filepath string) []string{
-	return []string{}
+import (
+	"io/ioutil"
+	"path/filepath"
+)
+
+func GetFiles(dirpath string) []string{
+	files, err := ioutil.ReadDir(dirpath)
+	if err != nil {
+		panic(err)
+	}
+
+	var paths []string
+	for _, file := range files {
+		if file.IsDir() {
+			paths = append(paths, GetFiles(filepath.Join(dirpath, file.Name()))...)
+			continue
+		}
+
+		paths = append(paths, filepath.Join(dirpath, file.Name()))
+
+	}
+
+	return paths
 }
