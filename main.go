0d58aaee3f1f5c24030e8ed44f1f04110b77133c
yoshikawa0711 <b1017166@fun.ac.jp>
2021-06-29 13:23:33 +0900

    [wip]FUSEを実装

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"log"
+	"time"
 
+	"github.com/hanwen/go-fuse/v2/fuse"
+	"github.com/hanwen/go-fuse/v2/fuse/nodefs"
 )
 
-func main() {
+type Root struct {
+	nodefs.Node
+}
 
+func (root *Root) GetAttr(out *fuse.Attr, file nodefs.File, ctx *fuse.Context) fuse.Status {
+	out.Mode = 0755
+	out.Atime = uint64(time.Now().Unix())
+	out.Mtime = uint64(time.Now().Unix())
+
+	return fuse.OK
+}
+
+func main() {
+	root := &Root{
+		Node: nodefs.NewDefaultNode(),
+	}
+
+	opts := nodefs.NewOptions()
+
+	s, _, err := nodefs.MountRoot("/mnt/fuse", root, opts)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	s.Serve()
 }
