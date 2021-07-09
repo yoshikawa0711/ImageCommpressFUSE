2af2775a3700424ef93bc8fc8ca6c741a1a678fb
yoshikawa0711 <b1017166@gmail.com>
2021-07-09 11:59:05 +0900

    [wip]OpenとReadの仮の関数を実装

--- makefuse/makefuse.go
+++ makefuse/makefuse.go
@@ -3,6 +3,7 @@ package makefuse
 import (
 	"flag"
 	"log"
+	"strconv"
 
 	"github.com/hanwen/go-fuse/v2/fuse"
 )
@@ -19,7 +20,7 @@ func NewTestFS() *TestFS {
 }
 
 // ファイルの属性が取得された時に，呼び出される関数
-func (fs *TestFS) GetAttr(cancel <-chan struct{}, imput *fuse.GetAttrIn, out *fuse.AttrOut) (code fuse.Status) {
+func (fs *TestFS) GetAttr(cancel <-chan struct{}, input *fuse.GetAttrIn, out *fuse.AttrOut) (code fuse.Status) {
 	log.Print("Get Attr.")
 	out.Mode = fuse.S_IFDIR | 0755
 
@@ -28,11 +29,26 @@ func (fs *TestFS) GetAttr(cancel <-chan struct{}, imput *fuse.GetAttrIn, out *fu
 }
 
 // ファイルが新しく作られた時に，呼び出される関数
-func (fs *TestFS) Create(cancel <-chan struct{}, imput *fuse.CreateIn, name string, out *fuse.CreateOut) (code fuse.Status) {
+func (fs *TestFS) Create(cancel <-chan struct{}, input *fuse.CreateIn, name string, out *fuse.CreateOut) (code fuse.Status) {
 	log.Print(name + " is Created.")
 
 	code = fuse.Status(0)
 	return code
+}
+
+// ファイルが開かれた時に，呼び出される関数
+func (fs *TestFS) Open(cancel <-chan struct{}, input *fuse.OpenIn, out *fuse.OpenOut) (status fuse.Status) {
+	log.Print(strconv.FormatUint(input.InHeader.NodeId, 10) + " is Opened.")
+
+	status = fuse.Status(0)
+	return status
+}
+
+func (fs *TestFS) Read(cancel <-chan struct{}, input *fuse.ReadIn, buf []byte) (fuse.ReadResult, fuse.Status) {
+	log.Print(strconv.FormatUint(input.InHeader.NodeId, 10) + " is Read.")
+	
+	status := fuse.Status(0)
+	return fuse.ReadResultData(buf), status
 }
 
 // FUSEを作る本体部分
